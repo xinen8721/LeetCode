60ced9e8e003c9b931403f48fd3156fd3544190a
xinen8721 <xinen8721@gmail.com>
2014-08-13 17:01:01 -0400

    Binary tree is balanced

---
+++ TreeIsBalanced.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        if(root.left==null && root.right==null){
+            return true;
+        }
+        if(Math.abs(depth(root.left)-depth(root.right)) > 1){
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+    
+    public int depth(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        return 1+Math.max(depth(root.left), depth(root.right));
+    }
+}
