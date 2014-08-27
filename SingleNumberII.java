07a59239a1b920dbf5039d1e91d893e5180741d4
xinen8721 <xinen8721@gmail.com>
2014-08-27 11:29:12 -0400

    SingleNumberII

---
+++ SingleNumberII.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int singleNumber(int[] A) {
+        int[] count = new int[32];
+        int result = 0;
+        
+        for(int i = 0; i < 32; i++)
+        {
+            for(int j = 0; j < A.length; j++)
+            {
+                if(((A[j] >> i) & 1) == 1)
+                {
+                    count[i]++;
+                }
+            }
+            result |= (count[i]%3)<<i;
+        }
+        
+        return result;
+    }
+}
